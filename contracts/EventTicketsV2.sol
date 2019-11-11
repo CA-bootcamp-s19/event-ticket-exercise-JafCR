99e8807f40332b245b1ecccb3216646f0176dc9d
Jaf <jaf@crbets.com>
2019-11-11 13:46:47 -0600

    com3

--- contracts/EventTicketsV2.sol
+++ contracts/EventTicketsV2.sol
@@ -9,7 +9,7 @@ contract EventTicketsV2 {
         Define an public owner variable. Set it to the creator of the contract when it is initialized.
     */
     address payable public owner;
-    uint   PRICE_TICKET = 1000000000000000000 wei;
+    uint   PRICE_TICKET = 100 wei;
 
     constructor() public payable {
         owner = msg.sender;
